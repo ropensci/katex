af0cecc1f7b0e2034324202e57db272d61d58caa
Jeroen Ooms <jeroenooms@gmail.com>
2021-09-11 10:30:56 +0200

    Better workaround for bug on Windows

--- R/katex.R
+++ R/katex.R
@@ -68,6 +68,8 @@ example_math <- function(){
 .onLoad <- function(lib, pkg){
   assign("ctx", V8::v8("window"), environment(.onLoad))
   ctx$source(system.file("js/katex.min.js", package = pkg))
+  ctx$source(system.file("js/he.min.js", package = pkg))
+  ctx$eval('function escape_utf8(rd){return he.encode(rd, {allowUnsafeSymbols: true})}')
 }
 
 katex_render <- function(tex, displayMode, ...) {
