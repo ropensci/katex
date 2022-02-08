983f66617aa73d62d0514ee4f0dfae63a297563f
Jeroen Ooms <jeroenooms@gmail.com>
2022-02-08 21:44:52 +0100

    render_math_in_html() now works with `mathjax --katex` output Fixes #7

--- inst/js/bindings.js
+++ inst/js/bindings.js
@@ -6,26 +6,22 @@ function html_render_math(html, include_css, options){
     var text = el.text().trim();
     /* pandoc --mathjax replaces $..$ with \(...\)  */
     var math = text.match(/\$([\s\S]*)\$$/) || text.match(/\\\(([\s\S]*)\\\)$/);
-    if(math && math[1]){
-      options.displayMode = false;
-      var out = katex.renderToString(math[1], options);
-      el.replaceWith(out);
-    } else {
-      console.warn("Did not find '$...$' or '\\(...\\)' equation in:\n" + text);
-    }
+    if(math && math[1])
+      text = math[1];
+    options.displayMode = false;
+    var out = katex.renderToString(text, options);
+    el.replaceWith(out);
   });
   $('.math.display').each(function(i, el){
     var el = $(el)
     var text = el.text().trim();
     /* pandoc --mathjax replaces $$..$$ with \[...\]  */
     var math = text.match(/^\${2}([\s\S]*)\${2}$/) || text.match(/\\\[([\s\S]*)\\\]$/);
-    if(math && math[1]){
-      options.displayMode = true;
-      var out = katex.renderToString(math[1], options);
-      el.replaceWith(out);
-    } else {
-      console.warn("Did not find '$$...$$' or '\\[...\\]' equation in:\n" + text);
-    }
+    if(math && math[1])
+      text = math[1];
+    options.displayMode = true;
+    var out = katex.renderToString(text, options);
+    el.replaceWith(out);
   });
   if(include_css){
     $('head').append('<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css" data-external="1">');
